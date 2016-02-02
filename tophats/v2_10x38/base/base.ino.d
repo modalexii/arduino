37034f43df847bd36916dc425c1c1aa439847b71
modalexii <kyle@unsecu.re>
2016-02-02 18:55:57 -0500

    Initial version

---
+++ tophats/v2_10x38/base/base.ino.d
@@ -0,0 +1,37 @@
+base.ino.o: \
+ /tmp/build72e7e86f9f6984c59c1bbea7cb684a12.tmp/sketch/base.ino.cpp \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/libraries/OctoWS2811/OctoWS2811.h \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/cores/teensy3/Arduino.h \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/cores/teensy3/WProgram.h \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/cores/teensy3/avr/pgmspace.h \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/cores/teensy3/avr/interrupt.h \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/cores/teensy3/avr_functions.h \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/cores/teensy3/wiring.h \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/cores/teensy3/binary.h \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/cores/teensy3/core_id.h \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/cores/teensy3/core_pins.h \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/cores/teensy3/kinetis.h \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/cores/teensy3/pins_arduino.h \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/cores/teensy3/HardwareSerial.h \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/cores/teensy3/Stream.h \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/cores/teensy3/Print.h \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/cores/teensy3/WString.h \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/cores/teensy3/Printable.h \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/cores/teensy3/new.h \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/cores/teensy3/avr_emulation.h \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/cores/teensy3/usb_serial.h \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/cores/teensy3/usb_desc.h \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/cores/teensy3/usb_seremu.h \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/cores/teensy3/usb_keyboard.h \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/cores/teensy3/keylayouts.h \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/cores/teensy3/usb_mouse.h \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/cores/teensy3/usb_joystick.h \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/cores/teensy3/usb_midi.h \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/cores/teensy3/usb_rawhid.h \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/cores/teensy3/usb_flightsim.h \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/cores/teensy3/usb_mtp.h \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/cores/teensy3/usb_undef.h \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/cores/teensy3/elapsedMillis.h \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/cores/teensy3/Arduino.h \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/cores/teensy3/IntervalTimer.h \
+ /home/kyle/Arduino-1.6.6-Teensy/hardware/teensy/avr/cores/teensy3/DMAChannel.h
