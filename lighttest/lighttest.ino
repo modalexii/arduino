8987ca3e17dd27ee0ebcf6afa972380f3a4c2ac0
modalexii <kyle@unsecu.re>
2016-02-02 18:59:53 -0500

    initial version

---
+++ lighttest/lighttest.ino
@@ -0,0 +1,47 @@
+#include <OctoWS2811.h>
+
+const int ledsPerStrip = 150*2;
+#define LED_LAYOUT     0
+
+DMAMEM int displayMemory[ledsPerStrip*6];
+int drawingMemory[ledsPerStrip*6];
+
+const int config = WS2811_GRB | WS2811_800kHz;
+
+OctoWS2811 leds(ledsPerStrip, displayMemory, drawingMemory, config);
+
+void setup() {
+  leds.begin();
+  leds.show();
+}
+
+#define RED    0xFF0000
+#define GREEN  0x00FF00
+#define BLUE   0x0000FF
+#define YELLOW 0xFFFF00
+#define PINK   0xFF1088
+#define ORANGE 0xE05800
+#define WHITE  0xFFFFFF
+
+void loop() {
+  int microsec = 2000000 / leds.numPixels();  // change them all in 2 seconds
+
+  // uncomment for voltage controlled speed
+  // millisec = analogRead(A9) / 40;
+
+  colorWipe(RED, microsec);
+  colorWipe(GREEN, microsec);
+  colorWipe(BLUE, microsec);
+
+}
+
+void colorWipe(int color, int wait)
+{
+  for (int i=0; i < leds.numPixels(); i++) {
+    leds.setPixel(i, color);
+    leds.show();
+    //delayMicroseconds(wait);
+  }
+  delay(500);
+}
+
