86542a849138a7937317f67abd4d28438815b506
modalexii <kyle@unsecu.re>
2016-10-27 21:25:41 -0400

    add experimental interrupt-based mode button

--- jackets/base/base.ino
+++ jackets/base/base.ino
@@ -4,7 +4,7 @@ const int CUFF_R[] = {0,13};
 const int COLLAR_R[] = {14,26};
 const int COLLAR_L[] = {26,38};
 const int CUFF_L[] = {39,52};
-uint8_t brightness = 50; //0-255
+uint8_t brightness = 25; //0-255
 
 int LEN_CUFF_R = CUFF_R[1] - CUFF_R[0];
 const int LEN_CUFF_L = CUFF_L[1] - CUFF_L[0];
@@ -12,6 +12,13 @@ const int LEN_R = COLLAR_R[1] - CUFF_R[0];
 const int LEN_L = CUFF_L[1] - COLLAR_L[0];
 
 const int totalLEDs = 53;
+
+const int buttonPin = 23; 
+const int debounceMilis = 200; // time to ignore subsequent edges
+int lastRisingEdge = debounceMilis;
+int numCyclingModes = 4; // number of modes to cycle between
+volatile int mode = 0; // index to move to next function in list
+volatile bool modeChangePending = 0;
 
 // These parameters control the fire appearance
 unsigned int heat = totalLEDs / 1.5;
@@ -21,7 +28,7 @@ unsigned int cool = 500; // higher = redder
 unsigned char canvas[totalLEDs];
 extern const unsigned int fireColor[100];
 
-elapsedMillis frameTimer, durationTimer;
+elapsedMillis frameTimer, durationTimer, debounceTimer;
 
 DMAMEM int displayMemory[totalLEDs*6];
 int drawingMemory[totalLEDs*6];
@@ -30,56 +37,123 @@ const int config = WS2811_GRB | WS2811_800kHz;
 OctoWS2811 leds(totalLEDs, displayMemory, drawingMemory, config);
 
 void setup() {
+  Serial.begin(9600);
+  delay(1000);
   leds.begin();
   leds.show();
+  pinMode(buttonPin, INPUT_PULLUP);
+  attachInterrupt(buttonPin, onExtInterrupt23, FALLING);
 }
 
 void loop() {
-  divergeRainbowCycle(30);
-  //redBug(100);
-  //fire(5000);
-  //twinkle(10000,80);
+  loopstart:
+  runMode();
+  Serial.println("loop");
 }
 
-void redBug(uint16_t wait) {
-  for(int i=0; i<=LEN_L; i++) {
-    fillSection(CUFF_R, getRGBL(77,176,209));
-    fillSection(CUFF_L, getRGBL(77,176,209));
-    fillSection(COLLAR_R, getRGBL(77,176,209));
-    fillSection(COLLAR_L, getRGBL(77,176,209));
-    leds.setPixel(COLLAR_R[1]-i,getRGBL(235,0,0));
-    leds.setPixel(COLLAR_L[0]+i,getRGBL(235,0,0));
-    leds.show();
-    delay(wait);
+void runMode() {
+
+  Serial.println("runMode");
+  // remember to update global numCyclingModes!
+  if(mode == 0) {
+    redBug(100, 1);
   }
-  for(int i=0; i<=LEN_CUFF_L; i++) {
-    fillSection(CUFF_R, getRGBL(77,176,209));
-    fillSection(CUFF_L, getRGBL(77,176,209));
-    leds.setPixel(CUFF_R[1]-i,getRGBL(235,0,0));
-    leds.setPixel(CUFF_L[0]+i,getRGBL(235,0,0));
-    leds.show();
-    delay(wait);
+  else if(mode == 1) {
+    divergeRainbowCycle(30, 2);
+  }
+  else if(mode == 2) {
+    fire(5000);
+  }
+  else if(mode == 3) {
+    twinkle(10000,80);
+  }
+  // remember to update global numCyclingModes!
+}
+
+void onExtInterrupt23() {
+
+  cli();
+  modeChangePending = 1;
+  if(debounceTimer - lastRisingEdge < debounceMilis) {
+    sei();
+    return;
+  }
+  lastRisingEdge = millis();
+  debounceTimer = 0;
+  // increment mode index
+  if(mode < numCyclingModes - 1) {
+    mode++;
+  }
+  else {
+    mode = 0;
+  }
+  sei();
+
+}
+
+void redBug(uint16_t wait, int rpt) {
+  Serial.println("redbug");
+  for(int r=0; r<rpt; r++) {
+    for(int i=0; i<=LEN_L; i++) {
+      if(modeChangePending) {
+        modeChangePending = 0;
+        return;
+      }
+      fillSection(CUFF_R, getRGBL(77,176,209));
+      fillSection(CUFF_L, getRGBL(77,176,209));
+      fillSection(COLLAR_R, getRGBL(77,176,209));
+      fillSection(COLLAR_L, getRGBL(77,176,209));
+      leds.setPixel(COLLAR_R[1]-i,getRGBL(235,0,0));
+      leds.setPixel(COLLAR_L[0]+i,getRGBL(235,0,0));
+      leds.show();
+      delay(wait);
+    }
+    for(int i=0; i<=LEN_CUFF_L; i++) {
+      if(modeChangePending) {
+        modeChangePending = 0;
+        return;
+      }
+      fillSection(CUFF_R, getRGBL(77,176,209));
+      fillSection(CUFF_L, getRGBL(77,176,209));
+      leds.setPixel(CUFF_R[1]-i,getRGBL(235,0,0));
+      leds.setPixel(CUFF_L[0]+i,getRGBL(235,0,0));
+      leds.show();
+      delay(wait);
+    }
   }
 }
 
-void divergeRainbowCycle(uint8_t wait) {
-  uint16_t i, j;
-  for(j=0; j<256; j++) {
-    for(i=CUFF_R[0]; i<= COLLAR_R[1]; i++) {
-      leds.setPixel(i, Wheel(((i * 256 / LEN_R) + j) & 255));
+void divergeRainbowCycle(uint8_t wait, int rpt) {
+  Serial.println("drainbow");
+  for(int r=0; r<rpt; r++) {
+    uint16_t i, j;
+    for(j=0; j<256; j++) {
+      for(i=CUFF_R[0]; i<= COLLAR_R[1]; i++) {
+        leds.setPixel(i, Wheel(((i * 256 / LEN_R) + j) & 255));
+      }
+      for(i=COLLAR_L[0]; i<= CUFF_L[1]; i++) {
+        leds.setPixel(i, Wheel(((LEN_L-i * 256 / LEN_L) + j) & 255));
+      }
+      leds.show();
+      if(modeChangePending) {
+        modeChangePending = 0;
+        return;
+      }
+      delay(wait);
     }
-    for(i=COLLAR_L[0]; i<= CUFF_L[1]; i++) {
-      leds.setPixel(i, Wheel(((LEN_L-i * 256 / LEN_L) + j) & 255));
-    }
-    leds.show();
-    delay(wait);
   }
 }
 
 void fire(int duration) {
+  Serial.println("fire");
   // wrap animateFire() insted of doing this dierctly in loop()
+  leds.begin(); // black out
   durationTimer = 0;
   while(durationTimer < duration) {
+    if(modeChangePending) {
+      modeChangePending = 0;
+      return;
+    }
     if (frameTimer >= 100) {
       frameTimer = 0;
       animateFire();
@@ -88,9 +162,14 @@ void fire(int duration) {
 }
 
 void twinkle(int duration, int rate) {
+  Serial.println("twinkle");
   leds.begin(); // black out
   durationTimer = 0;
   while(durationTimer < duration) {
+    if(modeChangePending) {
+      modeChangePending = 0;
+      return;
+    }
     if (frameTimer >= rate) {
       frameTimer = 0;
       animateTwinkle();
@@ -188,18 +267,6 @@ void fillSection(const int section[], uint32_t color) {
     leds.setPixel(i,color);
   }
   leds.show();
-}
-
-void rainbowCycle(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256; j++) { 
-    for(i=0; i< totalLEDs; i++) {
-      leds.setPixel(i, Wheel(((i * 256 / totalLEDs) + j) & 255));
-    }
-    leds.show();
-    delay(wait);
-  }
 }
 
 uint32_t getRGBL(uint8_t r, uint8_t g, uint8_t b) {
