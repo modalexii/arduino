8e6b06c2a5653a323e58d7f6d49b18909fb1847e
Kyle <kyle.c.steely@gmail.com>
2017-05-13 12:36:34 -0400

    readability and fix 3 relay cycle

--- laser_lounge/laser_lounge.ino
+++ laser_lounge/laser_lounge.ino
@@ -1,27 +1,30 @@
 #define minute 60000
+#define RL1 7
+#define RL2 6
+#define RL3 5
+#define RL4 4
+#define MODE 8
 
 void setup() {                
   // initialize the digital pins as an output.
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
+  pinMode(RL1, OUTPUT);
+  pinMode(RL2, OUTPUT);
+  pinMode(RL3, OUTPUT);
+  pinMode(RL4, OUTPUT);
+  pinMode(MODE, INPUT_PULLUP);
   test(); 
 }
 
 void test() {
-  for(int i = 4; i <= 7; i++) {
-    digitalWrite(i, HIGH);
-    delay(1000);
-    digitalWrite(i, LOW);
+  // if pin 8 is grounded, turn all relays on forever
+  if (digitalRead(MODE) == LOW ) {
+    digitalWrite(RL1, HIGH);
+    digitalWrite(RL2, HIGH);
+    digitalWrite(RL3, HIGH);
+    digitalWrite(RL4, HIGH);
+    while(true) {}
   }
-  for(int i = 4; i <= 7; i++) {
-    digitalWrite(i, HIGH);
-  }
-  delay(1000);
-  for(int i = 4; i <= 7; i++) {
-    digitalWrite(i, LOW);
-  }
+  // else just delay a moment
   delay(1000);
 }
 
@@ -46,43 +49,44 @@ void loop() {
   digitalWrite(4, LOW);
   */
 
+  // turn on #1
+  digitalWrite(RL1, HIGH);
+
+  // wait
+  delay(3 * minute);
+
   // turn on #2
-  digitalWrite(5, HIGH);
+  digitalWrite(RL2, HIGH);
+
+  // wait
+  delay(2 * minute);
+
+  // turn off #1
+  digitalWrite(RL1, LOW);
 
   // wait
   delay(3 * minute);
 
   // turn on #3
-  digitalWrite(6, HIGH);
+  digitalWrite(RL3, HIGH);
 
   // wait
   delay(2 * minute);
 
   // turn off #2
-  digitalWrite(5, LOW);
-
-  // wait
-  delay(3 * minute);
-
-  // turn on #4
-  digitalWrite(7, HIGH);
-
-  // wait
-  delay(2 * minute);
-
-  // turn off #3
-  digitalWrite(6, LOW);
+  digitalWrite(RL2, LOW);
 
   // wait
   delay(3 * minute);
 
   // turn on #1
-  digitalWrite(4, HIGH);
+  digitalWrite(RL1, HIGH);
 
   // wait
   delay(2 * minute);
 
-  // turn off #4
-  digitalWrite(7, LOW);
+  // turn off #3
+  digitalWrite(RL3, LOW);
+
  
 }
